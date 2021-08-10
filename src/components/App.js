8b16ae85f9c3852490217fd13532ce26cc577f2f
AJAY627 <ajay41527@gmail.com>
2021-08-10 13:49:25 +0530

    persisting-user

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,8 @@ import PropTypes from 'prop-types';
 
 import { fetchPosts } from '../actions/posts';
 import { Home, Navbar, Page404, Login } from './';
+import * as jwtDecode from 'jwt-decode';
+import { authenticateUser } from '../actions/auth';
 
 const SignUp = () => <div>SignUp</div>;
 
@@ -13,6 +15,20 @@ const SignUp = () => <div>SignUp</div>;
 class App extends React.Component {
   componentDidMount() {
     this.props.dispatch(fetchPosts());
+    const token = localStorage.getItem('token');
+
+    if (token) {
+      const user = jwtDecode(token);
+
+      console.log('user', user);
+      this.props.dispatch(
+        authenticateUser({
+          email: user.email,
+          _id: user._id,
+          name: user.name,
+        })
+      );
+    }
   }
 
   render() {
