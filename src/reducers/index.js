7be3204d702038a91a6758ba2af2d9d50d71130f
AJAY627 <ajay41527@gmail.com>
2021-08-12 19:35:32 +0530

    Searching users -1

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,10 +3,12 @@ import posts from './posts';
 import auth from './auth';
 import profile from './profile';
 import friends from './friends';
+import search from './friends';
 
 export default combineReducers({
   posts,
   auth,
   profile,
   friends,
+  search,
 });
